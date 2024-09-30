094ba92433ee8d2cd37a1aa89ec5c1a0ad4a1022
Omer Basar <78821009+omerfarukbasar@users.noreply.github.com>
2024-09-30 18:18:14 -0400

    elastic stuff

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%4ilw_+-j($rol#c)2$)8nu9z-#(8nz1hc&rwvhl(xh!@=8v&@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['swe-polls-dev.us-east-1.elasticbeanstalk.com']
 
 
 # Application definition
